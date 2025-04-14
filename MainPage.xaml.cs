1c6a10c21b7c3f178ca1c0a883192a5605e763d7
Dawid Bester <dawid@bester.bz>
2025-04-14 17:51:02 +1200

    Commented out workaround code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -24,11 +24,11 @@
                 // Code should scroll to Button66 row, but on iOS scroll to the row and moves the grid to the right
                 await TestScrollView.ScrollToAsync(Button66, ScrollToPosition.MakeVisible, false);
 
-                if (DeviceInfo.Platform == DevicePlatform.iOS)
-                {
-                    // workaround for MAUI makevisible BUG where scrolling scoresheet to the right when scrol performed
-                    await TestScrollView.ScrollToAsync(0, TestScrollView.ScrollY, false);
-                }
+                //if (DeviceInfo.Platform == DevicePlatform.iOS)
+                //{
+                //    // workaround for MAUI makevisible BUG where scrolling scoresheet to the right when scrol performed
+                //    await TestScrollView.ScrollToAsync(0, TestScrollView.ScrollY, false);
+                //}
             });
         }
     }
